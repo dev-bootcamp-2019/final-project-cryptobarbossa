9612d1796eb26409c8d45ebe734d87f7c03d3715
cryptobarbossa <cryptobarbossa@protonmail.com>
2019-01-20 22:04:24 +0000

    active checking

--- balance.py
+++ balance.py
@@ -3,5 +3,4 @@ with open('balance.txt', 'r') as myfile:
     data=myfile.read().replace('\n', '')
 vdata = int(data)
 if vdata > 0:
-	subprocess.Popen(["echo true > active.txt"])
-	subprocess.Popen(["./cfollow.sh"])
+	subprocess.Popen(["echo true > active.txt && ./cfollow.sh"],shell=True)
