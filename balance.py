d90757f6f4329459e75c3af7ab275fc453da1bd6
cryptobarbossa <cryptobarbossa@protonmail.com>
2019-01-20 21:56:24 +0000

    active check

--- balance.py
+++ balance.py
@@ -3,4 +3,5 @@ with open('balance.txt', 'r') as myfile:
     data=myfile.read().replace('\n', '')
 vdata = int(data)
 if vdata > 0:
+	subprocess.Popen(["echo true > active.txt"])
 	subprocess.Popen(["./cfollow.sh"])
--- balance.sh
+++ balance.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+value=$(<active.txt)
+
+if [ "$value" = "false" ]
+then
 node balance.js > balance.txt;
 python3 balance.py;
+fi
