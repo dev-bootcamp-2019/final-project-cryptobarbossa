c89831223487d405a752210d6c89bd200449affa
cryptobarbossa <cryptobarbossa@protonmail.com>
2019-01-27 14:32:56 +0000

    eth_agent_1

--- balance.js
+++ balance.js
@@ -2,16 +2,16 @@ var Web3 = require('web3');
 var web3 = new Web3();
 
 // Rinkeby ETH_AGENT_1 @ litecoin_official
-// const walletAddress = '0xffCD2130cb3DF4d55Df612BE664143EFcD0ff6B4';
-// web3.setProvider(new web3.providers.HttpProvider('https://rinkeby.infura.io/'));
+const walletAddress = '0xc01Af5741c52eA8dDa9166030a104f86CF181622';
+web3.setProvider(new web3.providers.HttpProvider('https://rinkeby.infura.io/'));
 
 // Rinkeby ETH_AGENT_2 @ monerocurrency
 // const walletAddress = '0x80479A14AD36607662A2631fB06F8ABe38F5126a';
 // web3.setProvider(new web3.providers.HttpProvider('https://rinkeby.infura.io/'));
 
 // Rinkeby ETH_AGENT_3 @ bitcoin_page
-const walletAddress = '0x58C57FB44cC4AF39C951DB2E06c1C6081Bd6efC8';
-web3.setProvider(new web3.providers.HttpProvider('https://rinkeby.infura.io/'));
+// const walletAddress = '0x58C57FB44cC4AF39C951DB2E06c1C6081Bd6efC8';
+// web3.setProvider(new web3.providers.HttpProvider('https://rinkeby.infura.io/'));
 
 // Used for local testing only
 // const walletAddress = '0xE5d210b3EC941C71C20096Dde60eE0e6dB74C0cC';
