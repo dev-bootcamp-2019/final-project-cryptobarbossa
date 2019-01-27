823aada145fa47f9b982f0a618c75605c5ee7d03
cryptobarbossa <cryptobarbossa@protonmail.com>
2019-01-27 13:40:09 +0000

    agent_1_ig

--- balance.js
+++ balance.js
@@ -1,10 +1,13 @@
 var Web3 = require('web3');
 var web3 = new Web3();
-// const walletAddress = '0x45539C89A9836399C45C3a8c0F01e8ae4EB43dFD';
-// web3.setProvider(new web3.providers.HttpProvider('https://rinkeby.infura.io/'));
 
-const walletAddress = '0xE5d210b3EC941C71C20096Dde60eE0e6dB74C0cC';
-web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:7545'));
+// Rinkeby ETH_AGENT_1 @ litecoin_official
+const walletAddress = '0xffCD2130cb3DF4d55Df612BE664143EFcD0ff6B4';
+web3.setProvider(new web3.providers.HttpProvider('https://rinkeby.infura.io/'));
+
+// Used for local testing only
+// const walletAddress = '0xE5d210b3EC941C71C20096Dde60eE0e6dB74C0cC';
+// web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:7545'));
 
 
 web3.eth.getBalance(walletAddress)
